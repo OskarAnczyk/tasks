f99d997699fae0913d117582b92fc8883d830bc0
Oskar Anczyk <myamy45.5@gmail.com>
2021-06-15 10:42:16 +0200

    heroku update 3

--- src/main/java/com/crud/tasks/trello/facade/TrelloFacade.java
+++ src/main/java/com/crud/tasks/trello/facade/TrelloFacade.java
@@ -8,11 +8,13 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import java.util.List;
 import java.util.stream.Collectors;
 
 @Component
+@CrossOrigin(origins = "*")
 public class TrelloFacade {
     @Autowired
     private TrelloService trelloService;
