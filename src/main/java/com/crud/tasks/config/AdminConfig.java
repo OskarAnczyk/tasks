aee1bd652a238a36d15549b3149033984322ac05
Oskar Anczyk <myamy45.5@gmail.com>
2021-06-22 12:44:41 +0200

    32.2 corrected

--- src/main/java/com/crud/tasks/config/AdminConfig.java
+++ src/main/java/com/crud/tasks/config/AdminConfig.java
@@ -12,13 +12,4 @@ public class AdminConfig {
 
     @Value("${admin.name}")
     private String adminName;
-
-    @Value("${info.company.name}")
-    private String companyName;
-
-    @Value("${info.company.email}")
-    private String companyEmail;
-
-    @Value("${info.company.phone}")
-    private String companyPhone;
 }
--- src/main/java/com/crud/tasks/service/MailCreatorService.java
+++ src/main/java/com/crud/tasks/service/MailCreatorService.java
@@ -1,6 +1,7 @@
 package com.crud.tasks.service;
 
 import com.crud.tasks.config.AdminConfig;
+import com.crud.tasks.config.CompanyConfig;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
@@ -15,6 +16,9 @@ public class MailCreatorService {
 
     @Autowired
     private AdminConfig adminConfig;
+
+    @Autowired
+    private CompanyConfig companyConfig;
 
     @Autowired
     @Qualifier("templateEngine")
@@ -34,9 +38,9 @@ public class MailCreatorService {
         context.setVariable("tasks_url", "http://localhost:8888/crud");
         context.setVariable("button", "Visit website");
         context.setVariable("admin_name", adminConfig.getAdminName());
-        context.setVariable("company_name", adminConfig.getCompanyName());
-        context.setVariable("company_email", adminConfig.getCompanyEmail());
-        context.setVariable("company_phone", adminConfig.getCompanyPhone());
+        context.setVariable("company_name", companyConfig.getCompanyName());
+        context.setVariable("company_email", companyConfig.getCompanyEmail());
+        context.setVariable("company_phone", companyConfig.getCompanyPhone());
         context.setVariable("show_button", false);
         context.setVariable("is_friend", true);
         context.setVariable("admin_config", adminConfig);
@@ -52,9 +56,9 @@ public class MailCreatorService {
         context.setVariable("tasks_url", "http://localhost:8888/crud");
         context.setVariable("button", "Visit website");
         context.setVariable("admin_name", adminConfig.getAdminName());
-        context.setVariable("company_name", adminConfig.getCompanyName());
-        context.setVariable("company_email", adminConfig.getCompanyEmail());
-        context.setVariable("company_phone", adminConfig.getCompanyPhone());
+        context.setVariable("company_name", companyConfig.getCompanyName());
+        context.setVariable("company_email", companyConfig.getCompanyEmail());
+        context.setVariable("company_phone", companyConfig.getCompanyPhone());
         context.setVariable("show_button", true);
         context.setVariable("is_friend", true);
         context.setVariable("database_entries",data_entries);
